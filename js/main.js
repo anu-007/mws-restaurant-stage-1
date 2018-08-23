4ddc7067c0f934b7d710b79018203eeedb6dde47
anu-007 <anubhutiv1@gmail.com>
2018-08-24 04:03:58 +0530

    style: improved responsive layout for homepage

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '',
+    mapboxToken: 'pk.eyJ1IjoiYW51LTAwNyIsImEiOiJjampsZ3M3dmQ0bXo2M2tuemoydG1mZmRuIn0.deeqjEUpXVKL8fGFcXLzMQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
